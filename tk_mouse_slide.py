b6e83caac2e337afd1d7b4c43c57863f45ae5ca6
Harsha-metmon <93405631+Harsha-metmon@users.noreply.github.com>
2022-04-06 22:46:42 +0200

    Add files via upload

--- tk_mouse_slide.py
+++ tk_mouse_slide.py
@@ -1,6 +1,8 @@
 from tkinter import *
 import time as time
 from numpy import pi
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Exiting GUI mainloop, and continuing with the program
 def creating():
@@ -16,8 +18,7 @@ def create_circle(x, y, r, canvas): #center coordinates, radius
     y0 = y - r
     x1 = x + r
     y1 = y + r
-    return canvas.create_oval(x0, y0, x1, y1)
-
+    return canvas.create_oval(x0, y0, x1, y1,fill='red')
 
 # Number of waypoints
 
@@ -31,9 +32,7 @@ ch=600
 def myfunction(event):
     root.counter += 1
     x, y = event.x, event.y
-    if root.counter<=N:
-     create_circle(x, y, 10, canvas)
-
+    
     if (root.old_coords and root.counter<=N):
         x1, y1 = root.old_coords
         canvas.create_line(x, y, x1, y1)
@@ -41,6 +40,9 @@ def myfunction(event):
     if root.counter<=N:
      root.old_coords = x, y
      coord.append((x, y))
+     create_circle(x, y, 10, canvas)
+
+
 
     if root.counter == N:
         print('maximum number of points that can be specified has reached thank you!!')
@@ -52,6 +54,7 @@ def myfunction(event):
 frame_a = Frame()
 Slider=[None]*N
 ang=[]
+global gui_variable
 gui_variable=[None]*N
 
 for i in range(N):
@@ -84,8 +87,49 @@ for i in range(N):
     ang_rad=ang_deg*(pi/180)
     ang.append(ang_rad)
 
+
 # mapping from local to world co-ordinates
 
+# moving the tp left origin canvas co-ordinates to bottom left cs
+
+for i in range(N):
+
+ coord[i][1]=ch-coord[i][1]
+
+
+# visualization
+
+#polygon dimensions
+
+wt=60
+ht=20
+
+
+points = np.array([[-0.5 * wt, -0.5 * ht], [+0.5 * wt, -0.5 * ht], [+0.5 * wt, +0.5 * ht], [-0.5 * wt, +0.5 * ht]])
+x_r = np.array([[0 for m in range(2)] for n in range(4)] , dtype='float')
+x_t = np.array([[0 for m in range(2)] for n in range(4)] , dtype='float')
+
+ax = plt.axes(aspect='equal')
+ax.set_xlabel('x(m)')
+ax.set_ylabel('y(m)')
+ax.set_title("robot position")
+
+for i in range(N):
+
+    for j in range(len(points)):
+        x_r[j, 0] = points[j, 0] * np.cos(ang[i]) - points[j, 1] * np.sin(ang[i])
+        x_r[j, 1] = points[j, 0] * np.sin(ang[i]) + points[j, 1] * np.cos(ang[i])
+        x_t[j, :] = x_r[j, 0] + coord[i][0], x_r[j, 1] + coord[i][1]
+
+    xx = x_t[:, 0]
+    xx = np.concatenate((xx, np.array([x_t[0, 0]])))
+    yy = x_t[:, 1]
+    yy = np.concatenate((yy, np.array([x_t[0, 1]])))
+
+    ax.plot(xx, yy)
+
+plt.show()
 
 print("the waypoints selected are: {}".format(coord))
+print("the waypoints selected are: {}".format(coord[0][1]))
 print("the angles selected are: {}".format(ang))
