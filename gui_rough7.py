588b72f04b88fdf96890ebf6734eb72b48c162ee
Harsha-metmon <93405631+Harsha-metmon@users.noreply.github.com>
2022-04-20 17:47:06 +0200

    Add files via upload

--- gui_rough7.py
+++ gui_rough7.py
@@ -15,9 +15,88 @@ from functools import partial
 import pdb as pdb
 
 
-# GUI+ROCKIT
+# GUI 1
+# This is done first and then another GUI opens for choreography design
 
-# GUI
+#THIS IS THE FIRST GUI THAT OPENS AND TAKES IN THE PARAMETER VALUSE AND THEN CLOSES#
+
+def creating():
+    root.destroy()
+
+# Creating GUI window
+root = Tk()
+root.title("Define parameters")
+
+# Creating mainframe to store widgets
+mainframe = Frame(root, width = 700, height = 500)
+mainframe.grid()
+mainframe.grid_propagate(1)
+
+# Defining labels for length and width
+Label(mainframe, text = "Length of room:").grid(row=1, column = 1)
+Label(mainframe, text = "Width of room:").grid(row=3, column = 1)
+Label(mainframe, text = "Number of robots:").grid(row=5, column = 1)
+# Defining variables for storing values from GUI
+gui_variable1=IntVar()
+gui_variable2=IntVar()
+gui_variable3=IntVar()
+
+# Creating two sliders to get user defined length and width
+Slider1 = Scale(mainframe, from_=1, to=15, length=600,tickinterval=1,variable=gui_variable1, orient=HORIZONTAL)
+Slider1.set(1)
+Slider1.grid(row =1, column = 2)
+Slider2 = Scale(mainframe, from_=1, to=15, length=600,tickinterval=1,variable=gui_variable2, orient=HORIZONTAL)
+Slider2.set(1)
+Slider2.grid(row = 3, column=2)
+Slider3 = Scale(mainframe, from_=1, to=4, length=600,tickinterval=1,variable=gui_variable3, orient=HORIZONTAL)
+Slider3.set(1)
+Slider3.grid(row = 5, column=2)
+
+# Executing button that closes GUI and continues with program
+Button(mainframe, text='select number of waypoints per robot', command=creating).grid(row = 6,columnspan = 3)
+
+# Waiting for button press
+root.mainloop()
+
+# Extracting values from sliders
+slider_value1= gui_variable1.get()
+slider_value2= gui_variable2.get()
+slider_value3= gui_variable3.get()
+
+## GUI 2.1 for number of waypoints per robot
+
+# Creating GUI window
+root = Tk()
+root.title("Define number of waypoints per robot in order")
+
+# Creating mainframe to store widgets
+mainframe = Frame(root, width = 1000, height = 1000)
+mainframe.grid()
+mainframe.grid_propagate(1)
+
+# Defining labels for length and width
+Label(mainframe, text = "Define number of waypoints per robot in order:").grid(row=0, column = 0)
+
+# Executing button that closes GUI and continues with program
+Button(mainframe, text='Design choregraphy', command=creating).grid(row = 6,columnspan = 3)
+
+
+gui_var=slider_value3*[None]
+Slider=slider_value3*[None]
+for i in range(slider_value3):
+    gui_var[i] = IntVar()
+    Slider[i] = Scale(master=mainframe, from_=2, to=4, length=300, tickinterval=1,
+                      variable=gui_var[i], orient=HORIZONTAL)
+    Slider[i].set(0)
+    Slider[i].grid(row=i + 1, column=1)
+
+root.mainloop()
+
+slider_val=slider_value3*[None]
+
+for i in range(slider_value3):
+    slider_val[i] = gui_var[i].get()
+
 
 # Exiting GUI mainloop, and continuing with the program
 def creating():
@@ -41,6 +120,9 @@ def create_rect(x, y, angle, canvas, tag,fil):  # center coordinates, radius
 
     elif(fil==2):
         fill='black'
+
+    elif(fil==3):
+        fill='yellow'
 
     else:
         fill='white'
@@ -68,8 +150,8 @@ def create_rect(x, y, angle, canvas, tag,fil):  # center coordinates, radius
     x_r = np.array([[0 for m in range(2)] for n in range(4)], dtype='float')
     x_t = np.array([[0 for m in range(2)] for n in range(4)], dtype='float')
 
-    xl = 1.5
-    yl = 1.5
+    xl = slider_value1
+    yl = slider_value2
 
     for j in range(len(points)):
         x_r[j, 0] = points[j, 0] * np.cos(angle) - points[j, 1] * np.sin(angle)
@@ -92,11 +174,11 @@ def create_rect(x, y, angle, canvas, tag,fil):  # center coordinates, radius
 
 # Number of waypoints
 
-Nw = [3,2,4]
+Nw = slider_val
 
 #Number of robots
 
-Nr=3
+Nr=slider_value3
 
 
 
@@ -110,7 +192,7 @@ coord=[[] for i in range(Nr)]
 cw = 600
 ch = 600
 
-t_g = [['rect1', 'rect2', 'rect3', 'rect4'],['rect12', 'rect22', 'rect32', 'rect42'],['rect13', 'rect23', 'rect33', 'rect43']]
+t_g = [['rect1', 'rect2', 'rect3', 'rect4'],['rect12', 'rect22', 'rect32', 'rect42'],['rect13', 'rect23', 'rect33', 'rect43'],['rect14', 'rect24', 'rect34', 'rect44']]
 
 
 global m
@@ -125,15 +207,15 @@ def myfunction(event):
         root.counter += 1
         x,y = event.x,event.y
         global m
+
         coord[m].append([x, y])
         lp_count = len(coord[m])
-        create_rect(x, y, 0, canvas, t_g[m][lp_count-1], 3)
+        create_rect(x, y, 0, canvas, t_g[m][lp_count-1], 1000)
 
         global n
 
         n += 1
-
-        if (n==Nw[m] and root.counter<(sum(Nw)-1)):
+        if (n==Nw[m] and root.counter<(sum(Nw))):
             m+=1
             n=0
 
@@ -145,7 +227,7 @@ def myfunction(event):
 
 def get_slider(event,l, k):
     ang[l][k] = gui_variable[l][k].get()
-    print(l,k,'lkkkkkkkkkkkkkkkkkkkkkkkkk')
+
     # kk = len(ang_l[k])
     ang_l[l][k].append(ang[l][k])
     if len(ang_l[l][k]) > 0:
@@ -154,7 +236,7 @@ def get_slider(event,l, k):
     # canvas.delete(t_g[k])
     create_rect(coord[l][k][-2], coord[l][k][-1], ang_l[l][k][-1], canvas, t_g[l][k], l)
     if k == (sum(Nw) - 1):
-        canvas.delete(t_g[l][Nw])
+        canvas.delete(t_g[l][Nw[k]])
 
 
 frame_a = Frame()
@@ -167,25 +249,26 @@ gui_variable = [[[] for j in range(Nw[i])] for i in range(Nr)]
 for j in range(Nr):
   for i in range(Nw[j]):
     gui_variable[j][i] = IntVar()
-    Slider[j][i] = Scale(master=frame_a, from_=0, to=360, length=600, tickinterval=30,
+    Slider[j][i] = Scale(master=frame_a, from_=0, to=360, length=300, tickinterval=30,
                       variable=gui_variable[j][i], orient=HORIZONTAL, command=partial(get_slider,l=j ,k=i))
     Slider[j][i].set(0)
     Slider[j][i].grid(row=i + 1, column=j+1)
 
+
 frame_b = Frame()
 canvas = Canvas(master=frame_b, width=cw, height=ch, bg='grey')
-canvas.create_line(300 - 20, 300, 300 + 20, 300)
-canvas.create_line(300, 300 - 20, 300, 300 + 20)
+canvas.create_line(cw/2 - 20, cw/2, cw/2 + 20, cw/2)
+canvas.create_line(ch/2, ch/2 - 20, ch/2, ch /2+ 20)
 canvas.bind("<Button-1>", myfunction)
 canvas.pack()
 
 frame_c = Frame()
-lb = Label(master=frame_c, text="select 4 angles using sliders", fg="red", bg="white", width=100, height=3)
+lb = Label(master=frame_c, text="select angles using sliders", fg="red", bg="white", width=100, height=3)
 
 lb.pack()
 
 frame_d = Frame()
-lb2 = Label(master=frame_d, text="select 4 points by clicking on grey canvas", fg="green", bg="white", width=100,
+lb2 = Label(master=frame_d, text="select points by clicking on grey canvas", fg="green", bg="white", width=100,
             height=3)
 lb2.pack()
 
@@ -198,77 +281,3 @@ frame_c.pack(side='top')
 root.old_coords = 0
 
 root.mainloop()
-
-# degrees to rad conversion
-
-'''
-for i in range(Nw):
-    ang[i] = ang[i] * (pi / 180)
-
-print(ang, 'final anglessssssssssssssssssssss')
-
-
-# mapping from local to world co-ordinates
-
-# moving the tp left origin canvas co-ordinates to bottom left cs
-
-# coord=list(coord)
-# print(coord)
-w_rm = 1
-h_rm = 1
-for i in range(Nw):
-    coord[i][1] = ch - coord[i][1]
-    coord[i][0] = (coord[i][0] - cw / 2) * (w_rm / cw)
-    coord[i][1] = (coord[i][1] - ch / 2) * (h_rm / ch)
-
-# call eagle here and extract location
-
-# points=[[coord[0][0],coord[0][1],ang[0]],[coord[1][0],coord[1][1],ang[1]],[coord[2][0],coord[2][1],ang[2]],[coord[3][0],coord[3][1],ang[3]]]
-coord_1 = [[coord[0][0], coord[0][1], ang[0]]]
-# print(coord_1,'coooooooooooooooooooords')
-# print(ang_l,'ang_lllllllllllllllllllllll')
-
-# visualization
-
-# polygon dimensions
-
-wt = 1
-ht = 0.3
-
-points = np.array([[-0.5 * wt, -0.5 * ht], [+0.5 * wt, -0.5 * ht], [+0.5 * wt, +0.5 * ht], [-0.5 * wt, +0.5 * ht]])
-x_r = np.array([[0 for m in range(2)] for n in range(4)], dtype='float')
-x_t = np.array([[0 for m in range(2)] for n in range(4)], dtype='float')
-
-xl = 1.5
-yl = 1.5
-
-ax = plt.axes(xlim=(-xl, +xl), ylim=(-yl, yl), aspect='equal')
-ax.set_xlabel('x(m)')
-ax.set_ylabel('y(m)')
-ax.set_title("Visualize the orientation and position of robot at 4 points")
-
-for i in range(Nw):
-
-    for j in range(len(points)):
-        x_r[j, 0] = points[j, 0] * np.cos(coord_1[i][2]) - points[j, 1] * np.sin(coord_1[i][2])
-        x_r[j, 1] = points[j, 0] * np.sin(coord_1[i][2]) + points[j, 1] * np.cos(coord_1[i][2])
-        x_t[j, :] = x_r[j, 0] + coord_1[i][0], x_r[j, 1] + coord_1[i][1]
-
-    xx = x_t[:, 0]
-    xx = np.concatenate((xx, np.array([x_t[0, 0]])))
-    yy = x_t[:, 1]
-    yy = np.concatenate((yy, np.array([x_t[0, 1]])))
-
-    ax.plot(xx, yy)
-# print(x_r)
-
-plt.show()
-
-# print("the waypoints selected are: {}".format(coord))
-# print("the angles selected are: {}".format(ang))
-
-# run eagle here and get the lacation to be the first point
-
-
-
-'''
